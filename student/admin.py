272318e48c63738aa69e55b424153deb5b19497d
StarveBread <vuntaliii@gmail.com>
2017-07-22 10:58:26 +0800

    initial commit

---
+++ student/admin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Detail
+from .models import Course
+from .models import Subject
+from .models import Professor
+
+class StudentAdmin(admin.ModelAdmin):
+	fieldset = [("Student Detail", {"field":["first_name","last_name"]})
+	]
+	
+def Student_Course(self, obj):
+	return obj.list_course()
+
+	
+list_display = ("first_name","last_name")
+
+admin.site.register(Course)
+admin.site.register(Detail)
+admin.site.register(Subject)
+admin.site.register(Professor)
+
+
---
+++ student/models.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Detail(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	middle_name = models.CharField(max_length=150)
+	age = models.IntegerField()
+	b_day = models.DateTimeField()
+	course = models.ManyToManyField("Course",related_name="Detail")
+	
+	
+	def __str__(self):
+		return "{} course {}".format(self.first_name,self.list_course())
+
+	def list_course(self):
+		return ",".join([course.course_name for course in self.course.all()])
+
+	def save(self, *args,**kwargs):
+		super(Detail,self).save(*args,**kwargs)
+
+
+
+
+class Course(models.Model):
+	course_name = models.CharField(max_length=150)
+	course_description = models.TextField(max_length=550)
+	subject = models.ManyToManyField("Subject",related_name="Courses")
+	
+	def __str__(self):
+		return self.course_name
+		
+		
+class Subject(models.Model):
+	subject_name = models.CharField(max_length=150)
+	subject_description = models.TextField(max_length=500)
+	
+	def __str__(self):
+		return self.subject_name
+		
+class Professor(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	middle_name = models.CharField(max_length=150)
+	course = models.ManyToManyField("Course",related_name="Professor")
+
+	
+	def __str__(self):
+		return self.first_name
+
+		
