16a8a7f20a562afc5cd0a13da3944287f50b103b
StarveBread <vuntaliii@gmail.com>
2017-08-05 11:31:24 +0800

    DetailViews added

--- student/views.py
+++ student/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+#from django.views.generic.detail import DetailView
+from django.utils import timezone
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, DetailView
 
 from .models import Detail
 from .models import Course
@@ -11,12 +13,21 @@ from .models import Faculty
 
 # Create your views here.
 def list_student(request):
-	students = Detail.objects.exclude()
-	context = {
-	'students' : students,
-	}
-	return render(request,"index.html",context)
+		students = Detail.objects.exclude()
+		context = {
+		'students' : students,
+		}
+		return render(request,"index.html",context)
+		
+
+class StudentDetail(DetailView):
+	model = Detail
+	template_name = "student.html"
 	
+	def get_context_data(self, **kwargs):
+		context = super(StudentDetail, self).get_context_data(**kwargs)
+		return context
+
 class courses_list(View):
 	def get(self,request):
 		courses = Course.objects.all()
@@ -41,9 +52,4 @@ class faculty_list(View):
 		}
 		return render(request,"Faculty.html",context)
 		
-def faculty_list_2(View):
-		faculties = Faculty.objects.exclude()
-		context = {
-		'faculty2' : faculties,
-		}
-		return render(request,"Faculty2.html",context)
+
